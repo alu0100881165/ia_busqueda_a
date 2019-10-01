38c4d2eeaf79a661bb9b9f17ae153944a93cb050
Daniel Nuez <alu0100881165@ull.edu.es>
2019-10-01 20:16:41 +0100

    Arreglado leer desde fichero, datos en un vector<int> para pasar al constructor del grafo.

--- main.cpp
+++ main.cpp
@@ -2,7 +2,10 @@
 #include <fstream>
 #include <iostream>
 #include <string>
-#include <sstream>
+#include <vector>
+#include <locale>
+#include <limits>
+#include <algorithm>
 
 using namespace std;
 
@@ -15,25 +18,74 @@ int main (int argc, char** argv)      //pongo char** para que almacene los nombr
   }
 
   ifstream graph_file(argv[1]), heuristic_file;
-  string graph_line, heuristic_line, string_aux;
-  istringstream graph_line_copy;
-  int tam = 0, cont = 0;
+  string graph_line, graph_line_fixed, heuristic_line, string_aux;
+  int tam = 0, int_aux = 0, int_dummy = 0, cont = 0;
 
   if(graph_file.is_open())
   {
     graph_file >> tam;
     cout << "Tamaño: " << tam << endl;
+
+    vector<vector<int>> nodos_fichero(tam, vector<int>(tam));
+
+    int_aux = tam;
+
+    while(int_aux > 0)
+    {
+      int_aux--;
+      int_dummy += int_aux;
+    }
+
+    vector<int> vector_int_aux(int_dummy);
+    // cout << "Tamaño del vector: " << nodos_fichero.size() << endl;
+    // for(int i = 0; i< nodos_fichero.size(); i++)
+    // {
+    //   cout << "Tamaño de los subvectores: " << nodos_fichero[i].size() << endl;
+    // }
+
     while(!graph_file.eof())
     {
-      getline(graph_file, graph_line);
-      cout << "Graph_line: " << graph_line << endl;
-      graph_line_copy.str(graph_line);
-      //cout << "Graph_line_copy" << graph_line_copy.str() << endl;
-      while(!graph_line_copy.eof())
+      getline(graph_file, graph_line, ' ');
+      if(graph_line.size() > 0)
       {
-        graph_line_copy >> string_aux;
-        cout << "Contenido: " << string_aux << endl;
+        if(graph_line[0] == '\n')
+          graph_line = graph_line.erase(0, 1);
+        // cout << "Graph_line 1: " << graph_line << endl;
+
+        for(int i = 0; i < graph_line.size(); i++)
+        {
+          if(isdigit(graph_line[i]))
+          {
+            graph_line_fixed.push_back(graph_line[i]);
+          }
+        }
+
+        // cout << "Graph_line 2: " << graph_line_fixed << endl;
+
+        if(isdigit(graph_line[0]))
+        {
+          int_aux = stoi(graph_line_fixed);
+          // cout << "Valor del int: " << int_aux << "   valor en el string: " << graph_line_fixed << endl;
+        }
+        else
+        {
+          int_aux = numeric_limits<int>::max();
+          // cout << "holi" << endl;
+        }
+
+        vector_int_aux[cont] = int_aux;
+        cont++;
+
+        cout << "Valor en entero: " << int_aux << endl;
+
+        getline(graph_file, graph_line);
+        //cout << "Graph_line 2: " << graph_line << endl;
+        graph_line_fixed.clear();
       }
+    }
+    for(int i = 0; i < vector_int_aux.size(); i++)
+    {
+      cout << "Valores del vector: " << vector_int_aux[i] << endl;
     }
   }
   else
